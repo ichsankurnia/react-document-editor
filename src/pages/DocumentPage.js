00d87a59871313e9a9faefb2aa6b11c77d4cadf6
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2021-12-01 23:50:29 +0700

    Update DocumentPage.js

--- src/pages/DocumentPage.js
+++ src/pages/DocumentPage.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { DocumentEditorContainerComponent , WordExport, SfdtExport, Selection, Editor, Toolbar } from '@syncfusion/ej2-react-documenteditor';
-import file from './../file';
+import file, { docxFile, sfdtFile } from './../file';
 
 //Inject require module.
 DocumentEditorContainerComponent.Inject(SfdtExport, Selection, Editor, WordExport, Toolbar);
@@ -11,25 +11,41 @@ export default class DocumentPage extends React.Component {
 	}
 
     save() {
-        this.documenteditor.documentEditor.save(this.state.filename || new Date(), 'Docx');
-		// this.documenteditor.documentEditor.saveAsBlob('Docx').then((res) => {
-		this.documenteditor.documentEditor.saveAsBlob('Sfdt').then((res) => {
+        // this.documenteditor.documentEditor.save(this.state.filename || new Date(), 'Docx');
+        // this.documenteditor.documentEditor.save(this.state.filename || new Date(), 'Sfdt');
+
+		this.documenteditor.documentEditor.saveAsBlob('Docx').then((res) => {
 			let reader = new FileReader();
-			reader.readAsDataURL(res);
+			reader.readAsDataURL(res);			// read as base64
+			// reader.readAsText(res)				// read as text
 			
 			reader.onloadend = () => {
 				let base64data = reader.result;
-				// this.createOrUpdateContract(base64data.toString());
 				console.log(base64data.toString())
 			};
+
+		}).catch(err => console.log('Err :', err))
+
+		this.documenteditor.documentEditor.saveAsBlob('Sfdt').then((res) => {
+			let reader = new FileReader();
+			reader.readAsText(res)				// read as text
+			
+			reader.onloadend = () => {
+				let base64data = reader.result;
+				console.log(base64data.toString())
+			};
+
 		}).catch(err => console.log('Err :', err))
     }
 
 	async componentDidMount(){
-		const doc = await this.urltoFile(file.data, 'test.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-		console.log(doc)
-
 		let fileReader = new FileReader();
+
+		const doc = await this.urltoFile(file.data, 'test.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+		fileReader.readAsText(doc);
+		
+		// const doc = await this.urltoFile(docxFile.data, 'test.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+		// fileReader.readAsDataURL(doc)
 
 		fileReader.onload = (e) => {
 			let contents = e.target.result;
@@ -37,10 +53,6 @@ export default class DocumentPage extends React.Component {
 			//Open the document in Document Editor.
 			proxy.documenteditor.documentEditor.open(contents);
 		};
-		//Read the file as text.
-		fileReader.readAsText(doc);
-
-		// this.documenteditor.documentEditor.open(doc)
 	}
 
 	urltoFile(url, filename, mimeType){
@@ -58,7 +70,7 @@ export default class DocumentPage extends React.Component {
 					isReadOnly={false} enableSelection={true} enableEditor={true} enableSfdtExport={true} enableWordExport={true}
 					serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" 
 				/>
-				<input onChange={(e)=>this.setState({filename: e.target.value})} value={this.state.filename}  />
+				<input onChange={(e)=>this.setState({filename: e.target.value})} value={this.state.filename}  className='outline-none border-1 rounded px-3 py-2' />
                 <button onClick={this.save.bind(this)}>Save</button>
             </div>);
     }
