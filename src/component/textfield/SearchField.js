81d0432960a2a8b4acafd8414eae3cdc50523fad
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-10 23:43:21 +0700

    Create SearchField.js

---
+++ src/component/textfield/SearchField.js
@@ -0,0 +1,15 @@
+const SearchField = ({placeholder, onChange}) => {
+    return (
+        <label className="relative block">
+            <span className="sr-only">Search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2.5">
+                <i className="ri-search-2-line text-gray-500 text-xl" />
+            </span>
+            <input className="placeholder-gray-400 block bg-white w-full border border-gray-300 rounded-2xl py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-red-800 focus:ring-red-800 focus:ring-1" 
+                onChange={onChange} placeholder={placeholder || "Search for anything..."} type="text" name="search" 
+            />
+        </label>
+    )
+}
+
+export default SearchField
