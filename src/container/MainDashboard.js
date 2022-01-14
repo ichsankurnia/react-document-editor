13962324c32f48d9c0a5d211bb0fcaebc1e74fd7
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-15 01:07:28 +0700

    update ui

--- src/container/MainDashboard.js
+++ src/container/MainDashboard.js
@@ -1,7 +1,27 @@
-const MainDashboard = () => {
+import { connect } from "react-redux"
+
+const MainDashboard = ({user}) => {
+    console.log(user)
     return (
-        <div>Main Dashboard</div>
+        <div className='flex item-center flex-col p-5 md:p-6 mb-auto'>
+            <h1 className='text-base font-semibold'>Dashboard</h1>
+
+            <div className="border-2 border-red-800 rounded-lg mt-5">
+                <div className="bg-red-800 p-5 text-white rounded-tl-md rounded-tr-md">
+                    <p>Welcome Back! <span className="font-semibold">{user?.e_fullname}</span></p>
+                </div>
+                <div className="bg-white px-5 py-6 rounded-bl-lg rounded-br-lg">
+                    <p>You are logged in!</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default MainDashboard
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, null) (MainDashboard)
--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,6 +1,8 @@
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div className='flex item-center flex-col p-5 md:p-6 mb-auto'>
+            <h1 className='text-base font-semibold'>Profile</h1>
+        </div>
     )
 }
 
