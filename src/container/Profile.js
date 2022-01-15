662abd66b7fe9be10316d44440b86b72527713da
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-16 01:36:51 +0700

    integration to api

--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,9 +1,261 @@
-const Profile = () => {
+import moment from "moment"
+import { useCallback, useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import { connect } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import { bindActionCreators } from "redux"
+import { updateUser, getOneUserByID, changePasswordUser } from "../api/user-api"
+import { ButtonCancel, ButtonSave } from "../component/button/CustomButton"
+import ErrorField from "../component/ErrorField"
+import Loader from "../component/modal/Loader"
+import ModalConfirm from "../component/modal/ModalConfirm"
+import ModalFormChangePassword from "../component/modal/ModalFormChangePassword"
+import { setUserData } from "../reduxs/action/actions"
+import Helper from "../utils/Helper"
+
+const BG_AVATAR = ['152e4d', '0891b2', '2E8B57', '8B4513', '4B0082', '999']
+
+const containerInput = 'flex flex-col w-full sm:w-1/2'
+const inputText = 'outline-none border-1 border-gray-200 rounded-lg py-2 px-3 sm:p-3 mt-1 focus:red-800 focus:ring-card2 focus:ring-1'
+
+const Profile = ({user, setUserData}) => {
+    const [isUpdate, setIsUpdate] = useState(false)
+    const [loader, showLoader] = useState(false)
+    const [modalLogout, showModalLogout] = useState(false)
+    const [modalPassword, showModalPassword] = useState(false)
+
+    const navigate = useNavigate()
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        setValue,
+        clearErrors 
+    } = useForm({ criteriaMode: "all" });
+
+    const setdefaultValue = useCallback ((obj) => {
+        Object.keys(obj).map(key => {
+            return setValue(key, obj[key], { shouldValidate: true })
+        })
+    }, [setValue])
+
+
+    const fetchUser = useCallback(async () => {
+        showLoader(true)
+        const res = await getOneUserByID(localStorage.getItem('doc-token'), user.i_id)
+        
+        console.log('Fetch One User :', res)
+        showLoader(false)
+        if(res.data){
+            if(res.data.status === '00'){
+                setdefaultValue(res.data.data)
+                setUserData(res.data.data)
+            }else{
+                if(res.data.message){
+                    toast.error(res.data.message)
+                }else{
+                    toast.error(`${res.config?.url} ${res.status} ${res.statusText}`)
+                }
+            }
+        }else{
+            toast.error(`${res.config?.url} ${res.message}`)
+        }
+
+    }, [user.i_id, setUserData, setdefaultValue])
+
+    useEffect(() => {
+        fetchUser()
+    }, [fetchUser])
+
+
+    const onValid = async (dataform) => {
+        showLoader(true)
+        const {e_fullname, e_phone_number, e_email} = dataform
+        
+        const payload = {
+            e_fullname, 
+            e_phone_number: Helper.IDPhoneNumber(e_phone_number), 
+            e_email,
+            i_group: user.i_group
+        }
+
+        const res = await updateUser(user.i_id, payload)
+        
+        console.log('Update User :', res)
+        if(res.data){
+            if(res.data.status === '00'){
+                toast.success(res.data.message)
+                setIsUpdate(false)
+                fetchUser()
+            }else{
+                if(res.data.message){
+                    toast.error(res.data.message)
+                }else{
+                    toast.error(`${res.config?.url} ${res.status} ${res.statusText}`)
+                }
+            }
+        }else{
+            toast.error(`${res.config?.url} ${res.message}`)
+        }
+    }
+
+    const handleSubmitChangePassword = async (payload) => {
+        showLoader(true)
+
+        const res = await changePasswordUser(user.i_id, payload)
+        
+        console.log('Update password :', res)
+        showLoader(false)
+        if(res.data){
+            if(res.data.status === '00'){
+                toast.success('Password changed, please login again')
+                showModalPassword(false)
+                setTimeout(() => {
+                    handleLogout()
+                }, 500);
+            }else{
+                if(res.data.message){
+                    toast.error(res.data.message)
+                }else{
+                    toast.error(`${res.config?.url} ${res.status} ${res.statusText}`)
+                }
+            }
+        }else{
+            toast.error(`${res.config?.url} ${res.message}`)
+        }
+    }
+
+    const handleMovePage = () => {
+        window.open('http://100.104.216.52','_blank')
+    }
+
+    const handleReset = (e) => {
+        setIsUpdate(false)
+        setdefaultValue(user)
+        clearErrors()
+    }
+
+    const handleLogout = () => {
+        localStorage.clear()
+        navigate('/', {replace:true})
+    }
+
     return (
-        <div className='flex item-center flex-col p-5 md:p-6 mb-auto'>
-            <h1 className='text-base font-semibold'>Profile</h1>
+        <div className='flex item-center flex-col p-6 mb-auto'>
+            <div className='flex items-center justify-between mb-5'>
+                <h1 className='text-base font-semibold'>Profile</h1>
+            </div>
+
+            <div>
+                <div className='flex flex-col md:flex-row justify-between items-center'>
+                    <div className='flex flex-col md:flex-row items-center mb-1'>
+                        <span className='w-24 sm:w-30'>
+                            <LazyLoadImage src={`https://ui-avatars.com/api/?name=${user.e_fullname}&background=${BG_AVATAR[Math.floor(Math.random() * BG_AVATAR.length)]}&color=fff&rounded=true&size=128`} effect='blur' />
+                        </span>
+                        <div className='md:ml-5 flex flex-col items-center md:items-start text-center md:text-left'>
+                            <h1 className='font-bold text-xl sm:text-2xl'>{user.e_fullname}</h1>
+                            <h2 className='text-sm'>Join at {moment(user?.d_created_at).format('DD-MMM-YYYY mm:hh')}</h2>
+                        </div>
+                    </div>
+                    <div className='inline-flex text-sm'>
+                        <i className="ri-user-5-fill" />
+                        <p className='ml-2'>{user.n_group || 'Administrator'}</p>
+                    </div>
+                </div>
+
+                <div className='bg-white text-text rounded-2xl shadow-2xl p-6 mt-6'>
+                    <div className='flex justify-between items-center mb-6'>
+                        <h1 className='font-semibold'>Account Detail</h1>
+                        <span className={`${isUpdate && 'text-white'} hover:text-red-800 cursor-pointer text-lg`} onClick={() => setIsUpdate(!isUpdate)}><i className="ri-edit-2-fill" /></span>
+                    </div>
+                    <form onSubmit={handleSubmit(onValid)} onReset={handleReset} id='form-user'>
+                        <fieldset disabled={!isUpdate}>
+
+                        <div className='flex flex-col sm:flex-row mb-3'>
+                            <div className={containerInput}>
+                                <label>Fullname</label>
+                                <input type='text' className={inputText} {...register("e_fullname", { required: "Fullname is required."})} />
+                                <ErrorField errors={errors} name='e_fullname' />
+                            </div>
+                            <span className='mx-5 mb-3 sm:mb-0'></span>
+                            <div className={containerInput}>
+                                <label>Username</label>
+                                <input type='text' readOnly className={`${inputText} bg-transparent`} {...register("n_username", { required: "Fullname is required."})} />
+                            </div>
+                        </div>
+                        <div className='flex flex-col sm:flex-row mb-3'>
+                            <div className={containerInput}>
+                                <label>Phone Number</label>
+                                <input type='tel' className={inputText} 
+                                    {...register("e_phone_number", {
+                                        required: "Phone number is required.",
+                                        pattern: { value: /^\d+$/, message: "Phone number is number only." },
+                                        minLength: { value: 10, message: "Phone number must exceed 9 characters." },
+                                        maxLength: { value: 14, message: "Phone number too long." }
+                                    })}
+                                />
+                                <ErrorField errors={errors} name='e_phone_number' />
+                            </div>
+                            <span className='mx-5 mb-3 sm:mb-0'></span>
+                            <div className={containerInput}>
+                                <label>Email</label>
+                                <input type='email' name='e_email' className={inputText} 
+                                    {...register("e_email", {
+                                        required: "Email is required.",
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "Invalid email address."
+                                        }
+                                    })}
+                                />
+                                <ErrorField errors={errors} name='e_email' />
+                            </div>
+                        </div>
+
+                        {isUpdate &&
+                        <div className='flex justify-end mt-5 md:mt-3'>
+                            <ButtonCancel type='reset' onClick={handleReset} />
+                            <ButtonSave type='submit' />
+                        </div>
+                        }
+
+                        </fieldset>
+                    </form>
+
+                </div>
+
+                <div className='bg-white text-text rounded-2xl shadow-2xl p-6 mt-6'>
+                    <h1 className='font-semibold mb-6 text-white'>Help</h1>
+                    <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
+                        <button className='hover:bg-red-800 hover:text-white rounded-2xl px-4 py-2 font-medium' onClick={()=>showModalPassword(true)}>Change Password</button>
+                        <button className='hover:bg-red-800 hover:text-white rounded-2xl px-4 py-2 font-medium' onClick={handleMovePage}>Contact Us</button>
+                        <button className='hover:bg-red-800 hover:text-white rounded-2xl px-4 py-2 font-medium' onClick={handleMovePage}>FAQ</button>
+                        <button className='hover:bg-red-800 hover:text-white rounded-2xl px-4 py-2 font-medium' onClick={handleMovePage}>Information</button>
+                        <button className='hover:bg-red-800 hover:text-white rounded-2xl px-4 py-2 font-medium' onClick={() => showModalLogout(true)}>Logout</button>
+                    </div>
+                </div>
+
+                <div className='flex justify-center mt-8 mb-3'>Company Name v2.0.0</div>
+            </div>
+
+            {modalPassword && <ModalFormChangePassword onCancel={() => showModalPassword(false)} onSubmit={handleSubmitChangePassword} />}
+            {loader && <Loader />}
+            {modalLogout && <ModalConfirm message='Are you sure to logout?' onOK={handleLogout} onCancel={() => showModalLogout(false)} />}
         </div>
     )
 }
 
-export default Profile
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({setUserData}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
--- src/container/admin/Document.js
+++ src/container/admin/Document.js
@@ -2,43 +2,44 @@ import { useCallback, useEffect, useState } from "react"
 import { connect } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
-import ModalFormProject from "../../component/modal/ModalFormProject"
 import { ButtonAdd } from "../../component/button/CustomButton"
 import TableFull from "../../component/table/TableFull";
 import Loader from "../../component/modal/Loader"
-import DropdownTwoOption from "../../component/dropdown/DropTwoOption"
 
-import { createNewProject, deleteProject, getAllProject, updateProject } from "../../api/project-api"
 import SearchField from "../../component/textfield/SearchField"
 import { toast } from "react-toastify"
+import { deleteDocument, getAllDocument } from "../../api/document-api"
+import DropdownDocument from "../../component/dropdown/DropdownDocument"
 
 
 const Document = ({user}) => {
     const [loader, showLoader] = useState(false)
-    const [modalForm, showModalForm] = useState(false)
-    const [dataProject, setDataProject] = useState([])
+    const [dataDocument, setDataDocument] = useState([])
     const [filterData, setFilterData] = useState([])
-    const [isUpdate, setIsUpdate] = useState(false)
-    const [selectedData, setSelectedData] = useState(null)
 
     const navigate = useNavigate()
 
     const fetchData = useCallback(async () => {
         showLoader(true)
 
-        const res = await getAllProject()
+        const res = await getAllDocument()
         
-        console.log('Get Project :', res)
+        console.log('Get all document :', res)
         resetForm()
         if(res.data){
-            if(res.data.code === 0){
-                setDataProject(res.data.data)
+            if(res.data.status === '00'){
+                setDataDocument(res.data.data)
                 setFilterData(res.data.data)
-            }else if(res.data.code === 99){
+            }else if(res.data.status === '01'){
                 showLoader(true)
+                toast.info('Session expired, please login!')
                 navigate('/auth')
             }else{
-                toast.error(res.data.message)
+                if(res.data.message){
+                    toast.error(res.data.message)
+                }else{
+                    toast.error(`${res.config?.url} ${res.status} ${res.statusText}`)
+                }
             }
         }else{
             toast.error(`${res.config?.url} ${res.message}`)
@@ -50,55 +51,25 @@ const Document = ({user}) => {
     }, [fetchData])
 
 
-    const handleEdit = (selectedData) => {
-        setSelectedData(selectedData)
-        setIsUpdate(true)
-        showModalForm(true)
-    }
-
-    const handleReceiveDataForm = async (payload) => {
-        showLoader(true)
-        
-        let res = null
-        if(!isUpdate){
-            payload.created_by_var = user.fullname_var
-            res = await createNewProject(payload)
-        }else{
-            payload.updated_by_var = user.fullname_var
-            res = await updateProject(selectedData.id_seq, payload)
-        }
-
-        console.log('Create/Update Project :', res)
-        
-        if(res.data){
-            if(res.data.code === 0){
-                toast.success(res.data.message)
-                fetchData()
-            }else{
-                toast.error(res.data.message)
-                showLoader(false)
-            }
-        }else{
-            toast.error(`${res.config?.url} ${res.message}`)
-        }
-    }
-
-
-    const handleDeleteData = async (id_seq) => {
+    const handleDeleteData = async (data) => {
         showLoader(true)
 
-        const res = await deleteProject(id_seq)
+        const res = await deleteDocument(data.i_id)
 
-        console.log('Get Project :', res)
+        console.log('Get Document :', res)
         showLoader(false)
         if(res.data){
-            if(res.data.code === 0){
+            if(res.data.status === '00'){
                 toast.success(res.data.message)
                 fetchData()
-            }else if(res.data.code === 99){
+            }else if(res.data.status === '00'){
                 navigate('/auth')
             }else{
-                toast.error(res.data.message)
+                if(res.data.message){
+                    toast.error(res.data.message)
+                }else{
+                    toast.error(`${res.config?.url} ${res.status} ${res.statusText}`)
+                }
             }
         }else{
             toast.error(`${res.config?.url} ${res.message}`)
@@ -107,53 +78,57 @@ const Document = ({user}) => {
 
     const resetForm = () => {
         showLoader(false)
-        setIsUpdate(false)
-        showModalForm(false)
-        setSelectedData(null)
     }
 
     const handleSearch = (event) => {
         event.preventDefault()
 
-        const newData = [...dataProject]
+        const newData = [...dataDocument]
         if(event.target.value){
             const filtered = newData.filter(item => {
                 return (
-                    item.project_name_var.toLowerCase().includes(event.target.value.toLowerCase()) ||
-                    item.project_code_var.toLowerCase().includes(event.target.value.toLowerCase())
+                    item.e_tittle.toLowerCase().includes(event.target.value.toLowerCase()) ||
+                    item.c_document_code.toLowerCase().includes(event.target.value.toLowerCase()) ||
+                    item.c_desc.toLowerCase().includes(event.target.value.toLowerCase()) ||
+                    item.d_created_at.toLowerCase().includes(event.target.value.toLowerCase())
                 )
             });
 
             setFilterData(filtered)
         }else{
-            setFilterData(dataProject)
+            setFilterData(dataDocument)
         }
     }
 
     const columns = [
         {
-            Header: () => <span className='p-4'>Project Code</span>,
-            Footer: 'Name',
-            accessor: 'project_code_var',
+            Header: () => <span className='p-4'>Document Code</span>,
+            Footer: 'Document Code',
+            accessor: 'c_document_code',
             Cell: ({ value }) =>  <div className='text-left pl-4'>{value}</div>,
         },
         {
-            Header: 'Project Name',
-            Footer: 'Project Name',
-            accessor: 'project_name_var'
+            Header: 'Title',
+            Footer: 'Title',
+            accessor: 'e_tittle'
         },
         {
-            Header: 'Contributors',
-            Footer: 'Contributors',
-            accessor: 'contributor'
+            Header: 'Description',
+            Footer: 'Description',
+            accessor: 'c_desc'
+        },
+        {
+            Header: 'Created at',
+            Footer: 'Created at',
+            accessor: 'd_created_at'
         },
         {
             Header: 'Status',
             Footer: 'Status',
-            accessor: 'status_int',
+            accessor: 'b_active',
             Cell: ({value}) => (
-                parseInt(value)===1? 
-                <span className='bg-agroo1 text-agroo4 px-2 py-1 rounded-lg font-semibold'>Active</span>
+                value? 
+                <span className='bg-green-100 text-green-800 px-2 py-1 rounded-lg font-semibold'>Active</span>
                 :
                 <span className='bg-red-100 text-red-800 px-2 py-1 rounded-lg font-semibold'>Inactive</span>
             )
@@ -163,16 +138,15 @@ const Document = ({user}) => {
             Footer: 'Action',
             Cell: ({row}) => {
                 const data = row.original
-                return <DropdownTwoOption onEdit={() => handleEdit(data)} onDelete={()=>handleDeleteData(data)} />
+                return <DropdownDocument onDetail={()=>navigate('/dashboard/document-detail/'+data.i_id)} onDelete={() => handleDeleteData(data)} />
             }
         }
     ]
 
     return (
         <div className='flex item-center flex-col p-6 mb-auto'>
-            <div className='flex items-center justify-between'>
-                <h1 className='text-base font-semibold'>DOCUMENT</h1>
-                <h2 className='text-base font-medium text-right'>Project Management<span className='text-xs'> / </span><span className='text-sm'>Document</span></h2>
+            <div>
+                <h1 className='text-base font-semibold'>Document Management</h1>
             </div>
 
             <div className='bg-white rounded-2xl shadow-2xl p-6 mt-6'>
@@ -186,7 +160,6 @@ const Document = ({user}) => {
                 <TableFull dataTable={filterData} columnTable={columns} />
             </div>
 
-            {modalForm && <ModalFormProject data={selectedData} onCancel={resetForm} onSubmit={handleReceiveDataForm} />}
             {loader && <Loader />}
         </div>
     )
