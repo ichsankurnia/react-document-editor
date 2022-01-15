9e0b2e182ece44f9a56632f19bee38876252dd12
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-16 01:35:22 +0700

    Update document-api.js

--- src/api/document-api.js
+++ src/api/document-api.js
@@ -16,3 +16,132 @@ export const convertDocxToSFDT = async () => {
         else return JSON.parse(JSON.stringify(error))  
     }
 }
+
+export const getAllDocument = async () => {
+    try {
+        const data = await axios.get(baseURL + '/document', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const getOneDocument = async (docID) => {
+    try {
+        const data = await axios.get(baseURL + '/document/id/' + docID, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const getDocumentByUser = async (userID) => {
+    try {
+        const data = await axios.get(baseURL + '/document/user/' + userID, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const getTemplateDocument = async (docName) => {
+    try {
+        const data = await axios.get(baseURL + '/document/template/' + docName, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            responseType: 'blob'
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const createNewDocument = async (payload) => {
+    try {
+        const data = await axios.post(baseURL + '/document/create', payload, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const updateDocument = async (docID, payload) => {
+    try {
+        const data = await axios.patch(baseURL + '/document/' + docID, payload, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const deleteDocument = async (docID) => {
+    try {
+        const data = await axios.delete(baseURL + '/document/' + docID, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
+
+export const approveDocument = async (payload) => {
+    try {
+        const data = await axios.post(baseURL + '/document/approve', payload, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("doc-token")}`,
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        return data
+    } catch (error) {
+        if(error.response) return error.response
+        else return JSON.parse(JSON.stringify(error))  
+    }
+}
