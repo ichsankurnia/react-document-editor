c4658845c849f242b99d6cf57d402c170f4f0a6a
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-18 22:06:28 +0700

    Create DropdownActionParamConfig.js

---
+++ src/component/dropdown/DropdownActionParamConfig.js
@@ -0,0 +1,82 @@
+import { useEffect, useRef, useState } from "react";
+import ModalConfirm from "../modal/ModalConfirm";
+
+
+const DropdownActionParamConfig = ({onEdit, onDelete, titleActive, onActive}) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false)
+    const [confirmActive, showConfirmActive] = useState(false)
+    const [confirmDelete, showConfirmDelete] = useState(false)
+
+    const trigger = useRef(null);
+    const dropdown = useRef(null);
+
+    // close on click outside
+    useEffect(() => {
+        const clickHandler = ({ target }) => {
+        if (!dropdownOpen || dropdown.current?.contains(target) || trigger.current?.contains(target)) return;
+            setDropdownOpen(false);
+        };
+        document.addEventListener('click', clickHandler);
+        return () => document.removeEventListener('click', clickHandler);
+    });
+
+    // close if the esc key is pressed
+    useEffect(() => {
+        const keyHandler = ({ keyCode }) => {
+        if (!dropdownOpen || keyCode !== 27) return;
+            setDropdownOpen(false);
+        };
+        document.addEventListener('keydown', keyHandler);
+        return () => document.removeEventListener('keydown', keyHandler);
+    });
+
+    const handleEdit = () => {
+        setDropdownOpen(false)
+        onEdit()
+    }
+
+    const handleDelete = () => {
+        showConfirmDelete(false)
+        onDelete()
+    }
+
+    const handleActivateUser = () => {
+        showConfirmActive(false)
+        onActive()
+    }
+
+    return (
+        <div className="relative inline-flex justify-center item-center flex-row-reverse">
+            <button onClick={() => setDropdownOpen(!dropdownOpen)} className="relative z-5 block rounded-md focus:outline-none cursor-pointer">
+                <span className='text-gray-700 hover:text-red-800'><i className="ri-more-fill text-xl font-bold"></i> </span>
+            </button>
+
+            {dropdownOpen &&
+            <div className={`mr-1 md:m-0 md:absolute right-7 -bottom-10 bg-white rounded-md shadow-lg overflow-hidden p-1 border-1 border-gray-200`}>
+                <div ref={dropdown} onFocus={() => setDropdownOpen(true)} onBlur={() => setDropdownOpen(false)} >
+                    <div className="flex flex-col w-40 min-w-max text-left">
+                        <button onClick={()=>showConfirmActive(true)} className="px-2 py-2 text-sm text-gray-700 hover:bg-red-800 hover:text-white inline-flex rounded-md items-center">
+                            <i className={`${titleActive==='Activate'? 'ri-shield-check-fill': 'ri-close-circle-fill'} mr-3 mt-0.5`} />
+                            <p>{titleActive}</p>
+                        </button>
+                        <button onClick={handleEdit} className="px-2 py-2 text-sm text-gray-700 hover:bg-red-800 hover:text-white inline-flex rounded-md items-center">
+                            <i className='ri-edit-box-fill mr-3 mt-0.5' />
+                            <p>Edit</p>
+                        </button>
+                        <button onClick={() => showConfirmDelete(true)} className="mt-1 px-2 py-2 text-sm text-gray-700 hover:bg-red-800 hover:text-white inline-flex rounded-md items-center">
+                            <i className='ri-delete-bin-5-fill mr-3 mt-0.5' />
+                            <p>Delete</p>
+                        </button>
+                        
+                    </div>
+                </div>
+            </div>
+            }
+
+            {confirmDelete && <ModalConfirm message='Delete this config?' onOK={handleDelete} onCancel={() => showConfirmDelete(false)} />}
+            {confirmActive && <ModalConfirm message={titleActive==='Activate'?'Activate this config?':'Deactivate this config?'} onOK={handleActivateUser} onCancel={() => showConfirmActive(false)} />}
+        </div>
+    )
+}
+
+export default DropdownActionParamConfig
