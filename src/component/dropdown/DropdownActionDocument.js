8ed1e0216036bfe2d558c7575951980b9526593b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2022-01-18 22:06:22 +0700

    rename file

--- src/component/dropdown/DropdownActionDocument.js
+++ src/component/dropdown/DropdownActionDocument.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import ModalConfirm from "../modal/ModalConfirm";
 
 
-const DropdownActionDocument = ({onEdit, onDelete}) => {
+const DropdownActionDocument = ({onDetail, onDelete}) => {
     const [dropdownOpen, setDropdownOpen] = useState(false)
     const [confirmDelete, showConfirmDelete] = useState(false)
 
@@ -29,10 +29,6 @@ const DropdownActionDocument = ({onEdit, onDelete}) => {
         return () => document.removeEventListener('keydown', keyHandler);
     });
 
-    const handleEdit = () => {
-        setDropdownOpen(false)
-        onEdit()
-    }
 
     const handleDelete = () => {
         showConfirmDelete(false)
@@ -42,18 +38,18 @@ const DropdownActionDocument = ({onEdit, onDelete}) => {
     return (
         <div className="relative inline-flex justify-center item-center flex-row-reverse">
             <button onClick={() => setDropdownOpen(!dropdownOpen)} className="relative z-5 block rounded-md focus:outline-none cursor-pointer">
-                <span className='text-gray-500 text-lg hover:text-agroo5'><i className="ri-more-fill"></i> </span>
+                <span className='text-gray-500 hover:text-red-800'><i className="ri-more-fill font-bold text-xl"></i> </span>
             </button>
 
             {dropdownOpen &&
             <div className={`mr-1 md:m-0 md:absolute right-7 -bottom-13 bg-white rounded-md shadow-lg overflow-hidden p-1 border-1 border-gray-200`}>
                 <div ref={dropdown} onFocus={() => setDropdownOpen(true)} onBlur={() => setDropdownOpen(false)} >
-                    <div className="flex flex-col w-32">
-                        <button onClick={handleEdit} className="px-4 py-2 text-sm text-gray-700 hover:bg-agroo5 hover:text-white inline-flex rounded-md">
+                    <div className="flex flex-col w-36 min-w-max">
+                        <button onClick={onDetail} className="px-4 py-2 text-sm text-gray-700 hover:bg-red-800 hover:text-white inline-flex items-center rounded-md">
                             <i className='ri-eye-fill mr-3 mt-0.5' />
-                            <p>View Detail</p>
+                            <p>Detail</p>
                         </button>
-                        <button onClick={() => showConfirmDelete(true)} className="mt-1 px-4 py-2 text-sm text-gray-700 hover:bg-agroo5 hover:text-white inline-flex rounded-md">
+                        <button onClick={() => showConfirmDelete(true)} className="mt-1 px-4 py-2 text-sm text-gray-700 hover:bg-red-800 hover:text-white inline-flex items-center rounded-md">
                             <i className='ri-delete-bin-5-fill mr-3 mt-0.5' />
                             <p>Delete</p>
                         </button>
@@ -63,7 +59,7 @@ const DropdownActionDocument = ({onEdit, onDelete}) => {
             </div>
             }
 
-            {confirmDelete && <ModalConfirm message='Hapus data ini?' onOK={handleDelete} onCancel={() => showConfirmDelete(false)} />}
+            {confirmDelete && <ModalConfirm message='Delete this document?' onOK={handleDelete} onCancel={() => showConfirmDelete(false)} />}
         </div>
     )
 }
